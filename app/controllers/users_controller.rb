6d805943bbfba0e9f2e8214d75cdba206c6ec58e
jenmcphail <mcphail.jen@gmail.com>
2016-06-19 21:28:49 -0700

    adds library model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,11 +12,11 @@ class UsersController < ApplicationController
 
 
 	def create
-    	user_params = params.require(:user).permit(:first_name, :last_name, :email, :password)
-    	@user = User.create(user_params)
-
-    	redirect_to "/users"
-  	end
+    user_params = params.require(:user).permit(:first_name, :last_name, :email, :password)
+    @user = User.create(user_params)
+    login(@user)
+    redirect_to "/users/#{@user.id}"
+  end
 
 	def show
     	@user = User.find(params[:id])
