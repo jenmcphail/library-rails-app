e1d12b43b9b0aa723488c23f6c5750bcacb24a00
jenmcphail <mcphail.jen@gmail.com>
2016-06-19 20:20:22 -0700

    debugged log-in issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
 	has_secure_password
 
 	def self.confirm(params)
-		@user = User.find_by({email: params[:email]})
-		@user.try(:authenticate, params[:password])
-	end
+    	@user = User.find_by({email: params[:email]})
+    	@user.try(:authenticate, params[:password])
+  	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,13 @@ Rails.application.routes.draw do
 
   get "/users", to: "users#index", as: "users"
 
-  get 'users/new', to: 'users#new', as: 'new_user'
+  get "/users/new", to: "users#new", as: "new_user"
 
-  post 'users', to: 'users#create'
+  post "/users", to: "users#create"
 
   get 'users/:id', to: 'users#show'
 
-  get '/sign_in', to: 'sessions#new'
+  get "/sign_in", to: "sessions#new"
 
-  post 'sessions', to: 'sessions#create'
+  post "/sessions", to: "sessions#create"
 end
